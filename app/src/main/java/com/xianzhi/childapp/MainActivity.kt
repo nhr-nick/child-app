9452ead10fb81e55fbeffccc2fd8905c898d05de
nick123456789010 <2770155322@qq.com>
2026-07-26 22:53:21 +0800

    Initial commit: child control Android app with GitHub Actions build

--- app/src/main/java/com/xianzhi/childapp/MainActivity.kt
+++ app/src/main/java/com/xianzhi/childapp/MainActivity.kt
@@ -12,6 +12,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import android.graphics.Bitmap
 import android.graphics.Color
 import com.google.zxing.BarcodeFormat
